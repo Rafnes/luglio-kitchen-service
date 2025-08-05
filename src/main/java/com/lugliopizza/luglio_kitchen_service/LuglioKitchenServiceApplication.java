c07acd9c28e07255460e09f34479121f56d3b098
Rafnes <rafzograf@gmail.com>
2025-08-05 11:50:57 +0300

    initial commit

---
+++ src/main/java/com/lugliopizza/luglio_kitchen_service/LuglioKitchenServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lugliopizza.luglio_kitchen_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuglioKitchenServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuglioKitchenServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lugliopizza/luglio_kitchen_service/LuglioKitchenServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lugliopizza.luglio_kitchen_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuglioKitchenServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

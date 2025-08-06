f0982a71e129e7d3f28f13cf28d5e2c5b4a4394f
Rafnes <rafzograf@gmail.com>
2025-08-06 13:39:30 +0300

    refactor: изменены поля сущностей Pizza, Ingredient, добавлен Lombok

--- src/main/java/com/lugliopizza/luglio_kitchen_service/model/Ingredient.java
+++ src/main/java/com/lugliopizza/luglio_kitchen_service/model/Ingredient.java
@@ -1,19 +1,40 @@
 package com.lugliopizza.luglio_kitchen_service.model;
 
+import com.lugliopizza.luglio_kitchen_service.enums.IngredientType;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import java.util.UUID;
+import java.util.ArrayList;
+import java.util.List;
 
+
+@Getter
 @Entity
 @Table(name = "ingredients")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Ingredient {
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(name = "type")
+    @Enumerated(EnumType.STRING)
+    @Setter
+    private IngredientType type;
+
     @Column(name = "name", nullable = false)
+    @Setter
     private String name;
 
     @Column(name = "description", nullable = false)
     private String description;
+
+    @Column(name = "description", nullable = false)
+    @Setter
+    @ManyToMany(mappedBy = "ingredients")
+    private List<Pizza> pizzas = new ArrayList<>();
 }
--- src/main/java/com/lugliopizza/luglio_kitchen_service/model/Pizza.java
+++ src/main/java/com/lugliopizza/luglio_kitchen_service/model/Pizza.java
@@ -2,12 +2,18 @@ package com.lugliopizza.luglio_kitchen_service.model;
 
 import com.lugliopizza.luglio_kitchen_service.enums.DoughType;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Table(name = "pizza")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Pizza {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,9 +25,18 @@ public class Pizza {
     @Column(name = "description", nullable = false)
     private String description;
 
+    @Column(name = "price")
+    private double price;
+
     @Enumerated(EnumType.STRING)
     @Column(name = "dough_type", nullable = false)
     private DoughType doughType;
 
+    @ManyToMany
+    @JoinTable(
+            name = "pizza_ingredient",
+            joinColumns = @JoinColumn(name = "pizza_id"),
+            inverseJoinColumns = @JoinColumn(name = "ingredient_id")
+    )
     private List<Ingredient> ingredients = new ArrayList<>();
 }
